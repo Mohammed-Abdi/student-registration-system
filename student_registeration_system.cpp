f66ab5023d10e18c9b90c24e0084ca4d27f707b9
Mohammed Abdi <mammegentleman@gmail.com>
2025-05-09 05:11:40 +0300

    added functionality for login function

--- student_registeration_system.cpp
+++ student_registeration_system.cpp
@@ -36,6 +36,51 @@ void loadFromFile(){}
 
 void homePage(){}
 
-void login(){}
+void login(){
+    string username, password;
+    bool loginSuccessful = false;
+    int attempt = 3;
 
-int main(){}
+    cout << "\n══ Login ════════════════\n";
+
+    while(loginSuccessful == false && attempt != 0){
+        cout << "Enter Username: ";
+        cin >> username;
+        cout << "Enter Password: ";
+        cin >> password;
+    
+        if(username == "admin" && password == "admin"){
+            loginSuccessful = true;
+        } else {
+            loginSuccessful = false;
+            attempt--;
+
+            string isPlural = "attempt";
+
+            if(attempt > 1){
+                isPlural = "attempts";
+            }
+
+            if(attempt != 0){
+                cout << "═════════════════════════\n"
+                     << "Incorrect username or password\n"
+                     << "You have " << attempt <<  " " << isPlural << " left!\n"
+                     << "═════════════════════════\n";
+            }
+            
+        }
+        }
+
+        cout << "═════════════════════════\n";
+
+        if(loginSuccessful == true){
+            cout << "Login Successful!";
+        } else if(loginSuccessful == false){
+            cout << "System Locked! too many attempts!";
+        }
+    
+}
+
+int main(){
+    login();
+}
